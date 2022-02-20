09cc46171f8dd114fb9f61edbdaf4fe86c83e548
NgocDuc <77544906+ngcduc@users.noreply.github.com>
2022-02-20 23:29:45 +0700

    update

---
+++ project/src/java/entity/product.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class product {
+    private int id;
+    private String name;
+    private String image;
+    private double price;
+    private String title;
+    private String description;
+}
