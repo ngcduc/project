55455036c8ed28b7b3c4f5c35490056b8375ae39
NgocDuc <77544906+ngcduc@users.noreply.github.com>
2022-02-21 00:35:04 +0700

    update

--- project/src/java/entity/product.java
+++ project/src/java/entity/product.java
@@ -16,4 +16,70 @@ public class product {
     private double price;
     private String title;
     private String description;
+
+    public product() {
+    }
+
+    public product(int id, String name, String image, double price, String title, String description) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "product{" + "id=" + id + ", name=" + name + ", image=" + image + ", price=" + price + ", title=" + title + ", description=" + description + '}';
+    }
+    
 }
